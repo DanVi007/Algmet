d46343e811eb58196e04697c54e4693f6b511082
DanVi007 <danvitrinh@outlook.com>
2022-08-29 21:36:11 +0200

    cleanup on dronning problem and added counter for solutions

--- DronningProblem/dronningProblem.cpp
+++ DronningProblem/dronningProblem.cpp
@@ -1,39 +1,8 @@
 #include <iostream> //  cout
 #include <string>   //  string
 #include <vector>
-
 #include <set>
 using namespace std;
-
-class Position
-{
-public:
-    vector<int> availablePositions = {1, 2, 3, 4, 5, 6, 7, 8};
-    int positionTaken;
-
-    Position(int positonToBeTaken)
-    {
-
-        positionTaken = positonToBeTaken;
-    }
-
-    template <size_t N>
-    Position(int (&notAvailablePos)[N]) // https://stackoverflow.com/questions/28653967/range-based-for-loop-throws-a-compiler-error-with-array-parameter
-    {
-
-        for (int a : notAvailablePos)
-        {
-            for (int b : availablePositions)
-            {
-                if (a == b)
-                {
-                    availablePositions[b];
-                }
-            }
-        }
-    }
-};
-
 class DiagonalLine
 {
 public:
@@ -47,13 +16,14 @@ public:
     }
 };
 
+int amountOfSolutions = 0;
+
 /**
  * a is depth 0, b is depth 1 ...
- *
- *
  */
 void miniMax(vector<int> takenHorizontals, vector<DiagonalLine> takenDiagonals, int depth)
 {
+    //TODO: modify to take in range like thingy, so that you dont have to change the numbers for other n*n chess boards
     set<int> availablePositions = {1, 2, 3, 4, 5, 6, 7, 8};
     //set<int> availablePositions = {1, 2, 3, 4};
 
@@ -77,6 +47,7 @@ void miniMax(vector<int> takenHorizontals, vector<DiagonalLine> takenDiagonals, 
             cout << i << " , ";
         }
         cout << "\n--------------------\n";
+        amountOfSolutions++;
     }
     else if (!availablePositions.empty())
     {
@@ -87,7 +58,6 @@ void miniMax(vector<int> takenHorizontals, vector<DiagonalLine> takenDiagonals, 
             mTakenHorizontals.push_back(i);
             mTakenDiagonals.push_back(DiagonalLine(i + depth, -1));
             mTakenDiagonals.push_back(DiagonalLine(i - depth, 1));
-
             miniMax(mTakenHorizontals,mTakenDiagonals,depth +1);
         }
     }
@@ -97,76 +67,9 @@ int main()
 {
     vector<int> startHorizontal = {};
     vector<DiagonalLine> startDiagonal = {};
-
     miniMax(startHorizontal,startDiagonal,0);
 
+    cout << amountOfSolutions;
     return 0;
 
-/*
-    set<int> availablePositions = {1, 2, 3, 4, 5, 6, 7, 8};
-    // cout << availablePositions.erase(1);
-
-    vector<int> numbers = {1, 2, 3};
-
-    numbers.push_back(6);
-    */
-
-    /*
-        Position aPosition = Position(5);
-
-        cout <<  aPosition.positionTaken << endl;
-    */
-
-    // cout << a << " , " << b << " , " << c << " , " << endl;
-
-    /*
-    set<int> allNumbers;
-
-    allNumbers.insert(1);
-    allNumbers.insert(2);
-    allNumbers.insert(3);
-    allNumbers.insert(4);
-    allNumbers.insert(5);
-    allNumbers.insert(6);
-    allNumbers.insert(7);
-    allNumbers.insert(8);
-
-    for (int a = 1; a <= 8; a++)
-    {
-        set<int> availableNumbersA = allNumbers;
-        availableNumbersA.erase(a);
-
-        for (int b : availableNumbersA)
-        {
-            set<int> availableNumbersB = availableNumbersA;
-            availableNumbersB.erase(b);
-
-            for (int c : availableNumbersB)
-            {
-                set<int> availableNumbersC = availableNumbersB;
-                availableNumbersC.erase(c);
-
-                for (int d : availableNumbersC)
-                {
-                    set<int> availableNumbersD = availableNumbersC;
-                    availableNumbersD.erase(d);
-
-                    for (int e : availableNumbersD)
-                    {
-                        set<int> availableNumbersE = availableNumbersD;
-                        availableNumbersE.erase(e);
-
-                        for (int f : availableNumbersE)
-                        {
-                            set<int> availableNumbersF = availableNumbersE;
-                            availableNumbersF.erase(f);
-
-                            cout << a << " , " << b << " , " << c << " , " << d <<" , " << e << " , " << " , " << f << endl;
-                        }
-                    }
-                }
-            }
-        }
-    }
-    */
 };
