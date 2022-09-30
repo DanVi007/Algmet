16b7270071882f294e333c1a2ef55d12b6c5c701
DanVi007 <danvt@stud.ntnu.no>
2022-09-30 13:31:55 +0200

    interesting issue with oppgave 9

--- oppgaver/oppgave9/oppgave9.cpp
+++ oppgaver/oppgave9/oppgave9.cpp
@@ -2,35 +2,58 @@
 
 using namespace std;
 
-const int N = 10 ;
-
+const int N = 3;
 int row[N];
 
-void buildRow(){
-    for(int i = 0; i < N;i++){
-        row[i] = i +1;
+void buildRow()
+{
+    for (int i = 0; i < N; i++)
+    {
+        row[i] = i + 1;
     }
 }
 
-void display(const int row[]){
-    for(int i = 0 ; i < sizeof(row)/sizeof(int); i++){
-        cout <<  row[i] << " ";
+void display(const int row[])
+{
+    for (int i = 0; i < N; i++)
+    {
+        cout << row[i] << " ";
     }
-    cout << endl; 
+    cout << endl;
 }
 
-void permutateClass(int row[] , int indexToChange, int lastIndex){
-   if(indexToChange == lastIndex -1){
-    display(row); 
-   } else {
-    
-
-   } 
-
+void swap(int row[], int i, int j)
+{
+    int temp = row[i];
+    row[i] = row[j];
+    row[j] = temp;
 }
 
-int main(){
+void permutateClass(int row[], int start, int last)
+{
+    if (start == last )
+    {
+        display(row);
+    }
+    else
+    {
+        for (int i = start; start <= last; i++)
+        {
+            swap(row, start, i);
+            permutateClass(row, start + 1, last);
+            swap(row ,i, start);
+        }
+    }
+}
+
+int main()
+{
     buildRow();
-
+    display(row);
+    swap(row, 1,2);
+    swap(row, 2,1);
+    display(row);
+    cout << endl;
+    //permutateClass(row, 0, N -1);
     return 0;
 }
