6c9f1d20a18b91a93308fda5a90de05e66203f1c
DanVi007 <danvt@stud.ntnu.no>
2022-10-22 07:50:43 +0200

    oppgave 3

--- oppgaver/oblig5/oppgave3.cpp
+++ oppgaver/oblig5/oppgave3.cpp
@@ -19,6 +19,7 @@ int gDybde = 0 , // gDybde blir nivaa til den første noden som blir funnet
  * koden ser etter en blad node, går dermed å leter etter flere bladnoder og sammenligner 
  * verdien til nyfunnet bladnodene med verdien til den første bladnoden.
  */
+
 /*
 void erKomplettTre(Node* node) {
     if(node && gKomplettTre){ //Er det en reell node og er treet fortsatt komplett 
@@ -46,30 +47,95 @@ void erKomplettTre(Node* node) {
 }
 */
 
-// ser etter blad noder på en preorder måte 
+// ser etter enebarn på en preorder måte 
 /**
- * koden ser etter en blad node, går dermed å leter etter flere bladnoder og sammenligner 
- * verdien til nyfunnet bladnodene med verdien til den første bladnoden.
+    
+ * koden ser etter et enebarn node, går dermed å leter etter flere enebarn  og sammenligner 
+ * verdien til nyfunnet enebarn med verdien til den første enebarn .
+ * enebarn er noder med ingen søsken dvs. forelderen til enebarnet har kun et barn. 
  */
+/*
 void erKomplettTre(Node* node) {
-      gNivaa++;
+    gNivaa++;
     if(node && gKomplettTre){ //Er det en reell node og er treet fortsatt komplett 
-      if(!node-> left || !node->right ){ // det mangler et barn 
-        if(!gNivaaOpp){ // er det første ukomplette funnet? 
+      if(!node-> left || !node->right ){ // det er et enebarn  
+        if(!gNivaaOpp){ //  er det det første enebarnet funnet 
           gDybde = gNivaa;
           gNivaaOpp = true; 
-        } else if(abs(gNivaa - gDybde) > 1){ // det er ikke det første ukomplette funnet og nivaaet funnet her er mer enn 1 mer enn det første 
+        } else if(abs(gNivaa - gDybde) > 1){ // det er ikke det første enebarnet funnet og nivaaet funnet her er mer eller mindre enn 1 enn det første 
           gKomplettTre = false; 
           return; 
         }
       }
-
         erKomplettTre(node->left);// gjør det samme for ventre og høyre  
         erKomplettTre(node->right); 
-    
     }
-        gNivaa--;
+    gNivaa--;
+}
+*/
+
+// ser etter enebarn på en preorder måte 
+/**
+    
+ * koden ser etter et enebarn node, går dermed å leter etter flere enebarn  og sammenligner 
+ * verdien til nyfunnet enebarn med verdien til den første enebarn .
+ * enebarn er noder med ingen søsken dvs. forelderen til enebarnet har kun et barn. 
+ */
+/*
+void erKomplettTre(Node* node) {
+    gNivaa++;
+    if(node && gKomplettTre){ //Er det en reell node og er treet fortsatt komplett 
+       if(gNivaaOpp && abs(gNivaa - gDybde) > 1 ) {
+        gKomplettTre = false;
+        return; 
+       }
+
+      if((!node->right || !node->left) && gNivaaOpp && gDybde != gNivaa){
+          gKomplettTre = false;
+          return; 
+      }
+       
+      if(!node->left ){
+        if(!gNivaaOpp){
+          gNivaaOpp = true;
+          gDybde = gNivaa; 
+          return;
+        } else if(gDybde != gNivaa){
+          gKomplettTre = false;
+          return; 
+        }  
+      }
+      
+      erKomplettTre(node->left);
+      erKomplettTre(node->right);
+
+    }
+    gNivaa--;
 }
 
+*/
 
+void erKomplettTre(Node * node ){
+  gNivaa++; 
+  if(node && gKomplettTre){
+    if(!gNivaaOpp){
+      if(!node->left){
+        gNivaaOpp = true;
+        gDybde = gNivaa;
+      }
+    } else {
+      if((gNivaa != gDybde && !node->right) || abs(gNivaa - gDybde) > 1 ){
+        gKomplettTre = false;
+        return;
+      } else {
+        gNivaaOpp = false;
+      }
+      
 
+    }
+    erKomplettTre(node->left);
+    erKomplettTre(node->right);
+
+  }
+  gNivaa--;
+}
