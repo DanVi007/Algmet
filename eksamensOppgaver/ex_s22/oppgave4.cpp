8c065ad8c404b65c787d82b9db87383411b42993
DanVi007 <danvitrinh@outlook.com>
2022-10-31 13:29:08 +0100

    modified oppgave 4 after tests

--- eksamensOppgaver/ex_s22/oppgave4.cpp
+++ eksamensOppgaver/ex_s22/oppgave4.cpp
@@ -5,13 +5,13 @@ using namespace std;
 int tallene[1000000] ;
 
 void skrivSum(){
-    int totalSum = 0;
     int tallHittil = tallene[0];
+    int totalSum = tallHittil;
     int antallTall = 1;
     for(int i = 1; i < 1000000; i++) {
         if(tallene[i] >= tallHittil ) {
-            totalSum += tallene[i];
             tallHittil = tallene[i];
+            totalSum += tallHittil;
             antallTall++;
         }
     }
