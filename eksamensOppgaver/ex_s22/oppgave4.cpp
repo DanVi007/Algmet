8b4cc30d6f1153af3eacd0f40d53b93e9a787dae
DanVi007 <danvitrinh@outlook.com>
2022-10-31 12:42:00 +0100

    moved ex_s22 and changes to opgave 4

--- eksamensOppgaver/ex_s22/oppgave4.cpp
+++ eksamensOppgaver/ex_s22/oppgave4.cpp
@@ -6,13 +6,16 @@ int tallene[1000000] ;
 void skrivSum(){
     int totalSum = 0;
     int tallHittil = tallene[0];
+    int antallTall = 0;
     for(int i = 1; i < 1000000; i++) {
         if(tallene[i] >= tallHittil ) {
             totalSum += tallene[i];
             tallHittil = tallene[i];
+            antallTall++;
         }
     }
-    cout << totalSum << endl;
+    cout << "Totalsum: " << totalSum << endl;
+    cout << "Antall summert: " << antallTall << endl;
 }
 int main() {
     skrivSum();
