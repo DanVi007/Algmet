4deaaba685bd6846ffa8cb98819a45d1074697ce
DanVi007 <danvt@stud.ntnu.no>
2022-11-10 10:57:51 +0100

    finished case 4

--- algcase/case4.cpp
+++ algcase/case4.cpp
@@ -28,6 +28,25 @@ int snuOppNed(const int n){
 
   // snu alle tallene i intString ork å gjøre men ha en haug med switch
   // hvis det ikke er mulig return 0
+  for(int i = 0; i < lengthOfInt; i ++){
+    char number = intString[i];
+    switch(number){
+      case '6':
+       intString[i] = '9'; 
+       break;
+      case '9':
+       intString[i] = '6';
+       break;
+      case '1':
+       break;
+      case '0':
+       break;
+      case '8':
+       break;
+      default:
+       return 0;
+    }
+  }
   
   return stoi(intString); 
 }
