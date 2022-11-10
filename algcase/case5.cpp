48e2552a494164bfa41386c2f1cdf390f36026d8
DanVi007 <danvitrinh@outlook.com>
2022-11-10 20:42:44 +0100

    case 5

--- algcase/case5.cpp
+++ algcase/case5.cpp
@@ -5,7 +5,7 @@ using namespace std;
 const int N = 3; // M > N, M og N må være positive heltall 
 const int M = 5; // M > N, M og N må være positive heltall 
 
-int gOnsker[N][3]; 
+int gOnsker[N][3] = {{1,2,3} ,{1,2,3},{4,3,2}};
 int gBesteFordeling[M]; //indeks er gjenstand -1. og verdi er nummer på person
 
 
@@ -32,9 +32,9 @@ int totalPrioritetNaa;
  *    resultatet ble. Oppdaterer ettersom en bedre verdi er funnet.  
  *
  */
-void finnBesteFordeling(int person){
+void finnBesteFordeling1(int person){
   // en permutering er nådd så gGjenstand blir sjekket 
-  if(person == 4){ // dersom det har nådd en ferdig permutering
+  if(person == N+1){ // dersom det har nådd en ferdig permutering
     totalPrioritetNaa = 0; // resetter nåværende prioritet 
     for(int i = 0; i < M;i++) { // går gjennom gGjenstand
       int person = gGjenstand[i]; 
@@ -52,16 +52,53 @@ void finnBesteFordeling(int person){
     if(totalPrioritetNaa < besteTotalPrioritet){ // opdaterer 
       besteTotalPrioritet = totalPrioritetNaa;
       for(int i = 0; i < M; i++) {
-        gBesteFordeling = gGjenstand[i];
+        gBesteFordeling[i] = gGjenstand[i];
       }
     }
   } else {
     for(int i = 0; i < M; i++){
       if(gGjenstand[i] == 0){
         gGjenstand[i] = person;
-        finnBesteFordeling(person+1);
+        finnBesteFordeling1(person+1);
+        gGjenstand[i] = 0; // backtracker
       }
-      gGjenstand[i] = 0;
+    }
+  }
+}
+
+bool ferdig = false;
+/*
+ * 1. setter person til gGjenstand rekursivt. (permutering)
+ *
+ * 2. Dersom alle er satt blir det sjekket hva resultatet sammenlignet med 
+ *    resultatet ble. Oppdaterer ettersom en bedre verdi er funnet.  
+ * very unfinished 
+ *
+ */
+void finnBesteFordeling2(int person){
+  // en permutering er nådd så gGjenstand blir sjekket 
+  if(person == N+1){ // dersom det har nådd en ferdig permutering
+    totalPrioritetNaa = 0; // resetter nåværende prioritet 
+    if(totalPrioritetNaa < besteTotalPrioritet){ // opdaterer 
+      besteTotalPrioritet = totalPrioritetNaa;
+      for(int i = 0; i < M; i++) {
+        gBesteFordeling[i] = gGjenstand[i];
+      }
+    }
+  } else {
+    for(int i = 0; i < M; i++){
+      if(gGjenstand[i] == 0){
+        gGjenstand[i] = person;
+        for(int j = 0; j< 3;j++) {
+          if(gOnsker[person][j] == i+1) {
+            totalPrioritetNaa += j+1; 
+          }
+
+        }
+        
+        finnBesteFordeling2(person+1);
+        gGjenstand[i] = 0; // backtracker
+      }
     }
   }
 }
@@ -71,15 +108,22 @@ void finnBesteFordeling(int person){
 void nullStillVariabler(){
   besteTotalPrioritet = INT_MAX;
   totalPrioritetNaa = 0;
-  gOnsker = {};
-  gBesteFordeling = {};
+  //gBesteFordeling
 }
 
-void initOnsker(){
-  gOnsker = {{}}
+void printBestePrio(){
+  cout << "Beste total prioritet " << endl;
+  for(int i = 0;i < M; i++) {
+    cout << "vare: " << i+1 << " , " << "eier: " << gBesteFordeling[i] << endl; 
+  }
 }
+
 int main() {
 
+  nullStillVariabler();
+  finnBesteFordeling2(1);
+  printBestePrio();
+ cout << besteTotalPrioritet  << endl;
 
   return 0;
 }
