a5204d968d692d79828341e214c1bd7dc8b3f053
DanVi007 <danvt@stud.ntnu.no>
2022-11-10 11:09:10 +0100

    improved case4

--- algcase/case4.cpp
+++ algcase/case4.cpp
@@ -7,26 +7,7 @@ using namespace std;
 int snuOppNed(const int n){
   string intString = to_string(n);
   const int lengthOfInt = intString.length();
-  const int arrayStorelse = lengthOfInt; 
-  int hoyrePunkt; 
-  int venstrePunkt;
-  if(lengthOfInt % 2 == 0){
-    hoyrePunkt = lengthOfInt/2; 
-    venstrePunkt = hoyrePunkt-1;
-  } else {
-    hoyrePunkt = lengthOfInt/2;
-    venstrePunkt = hoyrePunkt;
-  }
 
-  for( hoyrePunkt; hoyrePunkt <lengthOfInt;hoyrePunkt++ ){
-    int midlertidlig = intString[venstrePunkt] ;
-    intString[venstrePunkt] = intString[hoyrePunkt];
-    intString[hoyrePunkt] = midlertidlig;
-    venstrePunkt--;
-  }
-
-
-  // snu alle tallene i intString ork å gjøre men ha en haug med switch
   // hvis det ikke er mulig return 0
   for(int i = 0; i < lengthOfInt; i ++){
     char number = intString[i];
@@ -48,6 +29,24 @@ int snuOppNed(const int n){
     }
   }
   
+  int hoyrePunkt; 
+  int venstrePunkt;
+  if(lengthOfInt % 2 == 0){
+    hoyrePunkt = lengthOfInt/2; 
+    venstrePunkt = hoyrePunkt-1;
+  } else {
+    hoyrePunkt = lengthOfInt/2;
+    venstrePunkt = hoyrePunkt;
+  }
+
+  for( hoyrePunkt; hoyrePunkt <lengthOfInt;hoyrePunkt++ ){
+    int midlertidlig = intString[venstrePunkt] ;
+    intString[venstrePunkt] = intString[hoyrePunkt];
+    intString[hoyrePunkt] = midlertidlig;
+    venstrePunkt--;
+  }
+
+
   return stoi(intString); 
 }
 
