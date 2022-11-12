c6e32ce31ce492e32523e81fe11a118a8e84d12d
DanVi007 <danvitrinh@outlook.com>
2022-11-12 13:29:24 +0100

    eks20_2 finished

--- examplesAttempt/eks20_2.cpp
+++ examplesAttempt/eks20_2.cpp
@@ -1,81 +1,148 @@
-#include <iostream> 
+#include <iostream>
+
+using namespace std;
 
 const int endXindex = 3;
-const int endYindex= 3;
+const int endYindex = 3;
 const int N = 4;
-int availableNumbers[N*N];
+int availableNumbers[N * N];
 const int maxNumber = 34;
 
+int solutionCounter = 0;
+
+//int board[N][N] = {{1,2,15,16},{13,14,3,4},{12,7,10,5},{8,11,6,0}};
 int board[N][N];
+
+void printBoard()
+{
+  for (int y = 0; y < N; y++)
+  {
+    for (int x = 0; x < N; x++)
+    {
+      cout << board[x][y] << " , ";
+    }
+    cout << endl;
+  }
+  cout << "------------" << endl;
+}
 
 // int sumRightDiagonal = 0;
 // int sumLeftDiagonal = 0;
 int sumHorizontal = 0;
 // int sumVertical = 0;
 
-void printAllPermutations(int startIndex){
-  int xIndex = startIndex % N; 
+void printAllPermutations(int startIndex)
+{
+  int xIndex = startIndex % N;
   int yIndex = startIndex / N;
+  // printBoard();
+  // cout << "----------" << endl;
+  // cout << "sum: " << sumHorizontal << endl;
+  // cout << "startIndex:" << startIndex << endl;
+  // cout << "x , y " << xIndex << " , " << yIndex << endl;
 
-  for(int i = startIndex; i < N*N; i++){
+  if (xIndex == 0)
+  {
+    sumHorizontal = 0;
+    // printBoard();
+    // cout << "--------------" << endl;
+    // cout << startIndex << endl;
+  }
+  if (startIndex == N * N)
+  {
+    //printBoard();
+    solutionCounter++;
+    return;
+  }
 
-    // checking for availableNumbers
-    for(int j = 0; j < N*N; j++){
-      if(availableNumbers[j] != 0){
-        int numberToPut = availableNumbers[j];
-        sumHorizontal += numberToPut;
+  // checking for availableNumbers
+  int localSumHorizontal = sumHorizontal;
+  for (int j = 0; j < N * N; j++)
+  {
+    if (availableNumbers[j] != 0)
+    {
+      int numberToPut = availableNumbers[j];
+      sumHorizontal += numberToPut;
 
-        bool valid = true;
-        if(sumHorizontal > maxNumber){
+      bool valid = true;
+      if (sumHorizontal > maxNumber)
+      {
+        valid = false;
+      }
+      // TODO: SHORTEN
+      if (yIndex == endYindex)
+      {
+        // checks vertical
+        if ((board[xIndex][0] + board[xIndex][1] + board[xIndex][2] + numberToPut) != maxNumber)
+        {
           valid = false;
         }
-        if(yIndex == endYindex) {
-          if((board[xIndex][0] + board[xIndex][1] + board[xIndex][2] 
-                + numberToPut) != maxNumber){
-            valid = false;
-          } 
-
-          if(xIndex == 0 && 
-              (board[3][0] + board[3][0] + 
-               board[3][0] + numberToPut) != maxNumber {
-
-          }
-
-
+        else if (xIndex == 0 && 
+                 (board[3][0] + board[2][1] +
+                  board[1][2] + numberToPut) != maxNumber) //checks diagonal right to left 
+        {
+          valid = false;
         }
-        if(valid) {
-          availableNumbers[j] = 0;
-          board[xIndex][yIndex] = numberToPut;
-          printAllPermutations(startIndex+1);
-
-          // backtrack
-          board[xIndex][yIndex] = 0;
-          availableNumbers[j] = numberToPut;
+        else if (xIndex == endXindex &&
+                 (board[0][0] + board[1][1] +
+                  board[2][2] + numberToPut) != maxNumber) //checks diagonal left to right 
+        {
+          valid = false;
         }
-        sumHorizontal -= numberToPut;
       }
 
+      if (xIndex == endXindex && sumHorizontal != maxNumber)
+      {
+        valid = false;
+      }
+      if (valid)
+      {
+        availableNumbers[j] = 0;
+        board[xIndex][yIndex] = numberToPut;
+        printAllPermutations(startIndex + 1);
+
+        // backtrack
+        board[xIndex][yIndex] = 0;
+        availableNumbers[j] = numberToPut;
+      }
+      if(sumHorizontal == 0){
+        sumHorizontal = localSumHorizontal;
+      } else {
+        sumHorizontal -= numberToPut;
+      }
     }
-
   }
-
-
 }
 
-void initAvailableNumbers(){
-  for(int i = 0;i < N*N;i++) {
-    availableNumbers[i] = i+1;
+void initAvailableNumbers()
+{
+  for (int i = 0; i < N * N; i++)
+  {
+    availableNumbers[i] = i + 1;
   }
-
 }
 
+void initAvailableNumbersTest(){
+  availableNumbers[8] = 9;
+}
 
-int main(){
+int main()
+{
   initAvailableNumbers();
+  //initAvailableNumbersTest();
+  /*
   availableNumbers[0] = 0;
   board[0][0] = 1;
+  cout << "board before: " << endl;
+  printBoard();
+  sumHorizontal = 1;
   printAllPermutations(1);
+  */
+  //sumHorizontal = 25;
+  //printAllPermutations(15);
+  sumHorizontal = 0;
+  printAllPermutations(0);
+  cout << solutionCounter << endl;
 
   return 0;
-
 }
