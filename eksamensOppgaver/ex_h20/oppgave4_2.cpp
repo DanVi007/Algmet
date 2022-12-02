48c92c3b5f440c0c6cfcb035ee3efa9ba04f37fc
DanVi007 <danvt@stud.ntnu.no>
2022-12-02 16:25:11 +0100

    egne oppgaver og oppgave 4_2 ex_h20

--- eksamensOppgaver/ex_h20/oppgave4_2.cpp
+++ eksamensOppgaver/ex_h20/oppgave4_2.cpp
@@ -6,21 +6,56 @@ int fjernDuplikater(int a[], const int n){
   if(n== 0 || n== 1) {
     return n;
   } 
-  int unikInt = a[1];
-  int antallUnike = 1;
+  int unikInt = a[1];//Verdien til en unik int.
+  int antallUnike = 1;// antall unike, brukes også som ny indeks til unik verdi.
 
   for(int i = 1; i < n;i++){
-    if(unikInt != a[i+1]){
-      unikInt = a[i+1];
-      antallUnike++;
-      a[antallUnike] = unikInt;
-    }else {
-      a[i+1] = 0;
+    if(unikInt != a[i+1]){ // er den neste unik? 
+      unikInt = a[i+1]; // da setter vi en ny unik
+      antallUnike++; // inkrementerer antall unike.
+      a[antallUnike] = unikInt;//setter den unike etter den forrige unike
+
+    } 
+    if(antallUnike != i+1 ){ // har vi overkjørt antallUnike?
+      a[i+1] = 0; // da fjerner vi den neste 
     }
   }
-  return antallUnike;
+  return antallUnike; // returnerer antall unike funnet.
+}
+
+// etter å ha sett på fasit
+int fjernDuplikater2(int a[], const int n){
+  if(n== 0 || n== 1) {
+    return n;
+  } 
+  int antallUnike = 1;// antall unike, brukes også som indeks til unik verdi.
+
+  for(int i = 1; i < n;i++){
+    if(a[antallUnike]!= a[i+1]){ // er den neste unik? 
+      antallUnike++; // inkrementerer antall unike.
+      a[antallUnike] = a[i+1] ;//setter den unike etter den forrige unike
+    } 
+    if(antallUnike != i+1 ){ // har vi overkjørt antallUnike?
+      a[i+1] = 0; // da fjerner vi den neste 
+    }
+  }
+  return antallUnike; // returnerer antall unike funnet.
+}
+
+void printArray(int a[],const int len ) {
+  for(int i = 0; i < len;i++) {
+    cout << a[i] << " , ";
+  }
+  cout << endl;
 }
 
 int main(){
+  int tall[15] = {0,1,2,2,2,3,4,5,5,5,7,9,10,11,11};
+  printArray(tall,15);
+  cout << fjernDuplikater2(tall,14) << endl;
+  printArray(tall,15);
+
+
+
 
 }
